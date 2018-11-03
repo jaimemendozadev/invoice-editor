a1685112fa3650927056a59a322e64a608bde04a
jaimemendozadev <jaimemendozadev@gmail.com>
2018-11-03 08:01:58 -0700

    Add Delete Image icon

--- dev/app/components/lineitem/index.jsx
+++ dev/app/components/lineitem/index.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import DeleteIcon from "../assets/delete-button.png";
 
 const LineItem = ({ lineItem }) => (
   <div className="line-item">
@@ -6,8 +7,8 @@ const LineItem = ({ lineItem }) => (
     <div className="line-item-field qty-field">{lineItem.qty}</div>
     <div className="line-item-field price-field">{lineItem.price}</div>
     <div className="line-item-field total-field">
-      {lineItem.total}
-      <span>X</span>
+      <div>{lineItem.total}</div>
+      <img src={DeleteIcon} alt="Delete Icon for Line Item" />
     </div>
   </div>
 );
--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,19 @@ module.exports = {
       {
         test: /\.(sa|sc|c)ss$/,
         use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          "file-loader",
+          {
+            loader: "image-webpack-loader",
+            options: {
+              bypassOnDebug: true, // webpack@1.x
+              disable: true // webpack@2.x and newer
+            }
+          }
+        ]
       }
     ]
   },
